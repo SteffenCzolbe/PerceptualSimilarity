Fixed data normalization

src/vae/celebA/train.py
@@ -49,7 +49,8 @@ batch_size = 64
 # key word args for loading data
 kwargs = {'num_workers': 1, 'pin_memory': True} if device == "cuda" else {}
 
-dataset = datasets.ImageFolder(dataset_path, transform=transforms.ToTensor())
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5),(0.5, 0.5, 0.5))])
+dataset = datasets.ImageFolder(dataset_path, transform=transform)
 data_size = len(dataset)
 train_size = int(0.9 * data_size)
 test_size = data_size - train_size
@@ -72,7 +73,14 @@ test_loader = torch.utils.data.DataLoader(
 
 
 # helper function
-to_img = transforms.ToPILImage()
+to_img_torch = transforms.ToPILImage()
+
+def to_img(x):
+    # rescale to 0..1
+    x = (x+1)/2 
+    img = to_img_torch(x)
+    return img
+    
 
 
 # In[ ]:

src/vae/mnist/evaluation.py
@@ -41,7 +41,8 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if device == "cuda" else {}
 transformers = transforms.Compose([
     transforms.Grayscale(num_output_channels=1),         # grayscale images
     transforms.Pad((data_dim[1] - 28) // 2, fill=0, padding_mode='constant'), # padding
-    transforms.ToTensor()                                # as tensors
+    transforms.ToTensor(),                               # as tensors
+    transforms.Normalize((0.5, 0.5, 0.5),(0.5, 0.5, 0.5))
 ])
 
 test_set = datasets.ImageFolder(dataset + 'test/', transform=transformers)
@@ -58,6 +59,8 @@ test_loader = torch.utils.data.DataLoader(
 to_img_torch = transforms.ToPILImage()
 
 def to_img(x):
+    # rescale to 0..1
+    x = (x+1)/2 
     img = to_img_torch(x)
     # upsampling. Otherwhgise pdf viewers will fuck with the interpolation
     w, h = img.size

src/vae/mnist/train.py
@@ -61,7 +61,8 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if device == "cuda" else {}
 transformers = transforms.Compose([
     transforms.Grayscale(num_output_channels=1),         # grayscale images
     transforms.Pad((data_dim[1] - 28) // 2, fill=0, padding_mode='constant'), # padding
-    transforms.ToTensor()                                # as tensors
+    transforms.ToTensor(),                               # as tensors
+    transforms.Normalize((0.5, 0.5, 0.5),(0.5, 0.5, 0.5))
 ])
 
 train_set = datasets.ImageFolder(dataset + 'train/', transform=transformers)
@@ -88,7 +89,14 @@ test_loader = torch.utils.data.DataLoader(
 
 
 # helper function
-to_img = transforms.ToPILImage()
+to_img_torch = transforms.ToPILImage()
+
+def to_img(x):
+    # rescale to 0..1
+    x = (x+1)/2 
+    img = to_img_torch(x)
+    return img
+    
 
 
 # In[6]:
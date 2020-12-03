db76748a3257677d01cdccb681c82278a6cf6cfe
Steffen <per.sc@di.ku.dk>
2020-12-03 18:11:06 +0100

    fixed command duplication

--- src/perceptual_sim_training/scripts/download_dataset_valonly.sh
+++ src/perceptual_sim_training/scripts/download_dataset_valonly.sh
@@ -2,7 +2,7 @@
 mkdir dataset
 
 # JND Dataset
-#wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz --no-check-certificate
+#wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz --no-check-certificate
 
 #mkdir dataset/jnd
 #tar -xzf ./dataset/jnd.tar.gz -C ./dataset
@@ -10,7 +10,7 @@ mkdir dataset
 
 # 2AFC Val set
 mkdir dataset/2afc/
-wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/twoafc_val.tar.gz -O ./dataset/twoafc_val.tar.gz wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz --no-check-certificate
+wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/twoafc_val.tar.gz -O ./dataset/twoafc_val.tar.gz --no-check-certificate
 
 mkdir dataset/2afc/val
 tar -xzf ./dataset/twoafc_val.tar.gz -C ./dataset/2afc

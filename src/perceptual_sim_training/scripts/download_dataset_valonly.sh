281c5a7e578928307fc238ed3b0d6bd61b93d847
Steffen <per.sc@di.ku.dk>
2020-12-03 18:05:29 +0100

    supress cert error, only download 2AFC

--- src/perceptual_sim_training/scripts/download_dataset_valonly.sh
+++ src/perceptual_sim_training/scripts/download_dataset_valonly.sh
@@ -2,15 +2,15 @@
 mkdir dataset
 
 # JND Dataset
-wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz
+#wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz --no-check-certificate
 
-mkdir dataset/jnd
-tar -xzf ./dataset/jnd.tar.gz -C ./dataset
-rm ./dataset/jnd.tar.gz
+#mkdir dataset/jnd
+#tar -xzf ./dataset/jnd.tar.gz -C ./dataset
+#rm ./dataset/jnd.tar.gz
 
 # 2AFC Val set
 mkdir dataset/2afc/
-wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/twoafc_val.tar.gz -O ./dataset/twoafc_val.tar.gz
+wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/twoafc_val.tar.gz -O ./dataset/twoafc_val.tar.gz wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz --no-check-certificate
 
 mkdir dataset/2afc/val
 tar -xzf ./dataset/twoafc_val.tar.gz -C ./dataset/2afc
